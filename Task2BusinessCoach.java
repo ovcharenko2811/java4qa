081d385af1af42293e91d843ba490e3b77194906
Sviatoslav Ovcharenko <sovcharenko@alfabank.ru>
2026-04-16 10:44:56 +0300

    task 2 - зафиналил, добавил комменты

--- Task2BusinessCoach.java
+++ Task2BusinessCoach.java
@@ -3,26 +3,29 @@ import java.util.Random;
 
 public class Task2BusinessCoach {
     public static void main(String[] args) {
-        Random random = new Random();
-        int age = random.nextInt(100);
-        double accountBalance = random.nextDouble(1000000);
-        boolean hasInvitation  = true;
-        boolean isBanned = false;
+        Random random = new Random(); // создаем экземпляр класса Random, чтобы использовать в статичном методе
+        int age = random.nextInt(100); // генерим возраст до 100
+        double accountBalance = random.nextDouble(1000000); // генерим баланс на счете до ляма, потом обрежем до сотых
+        boolean hasInvitation  = true; // есть ли у посетителя приглашение, хардкодим согласно постановке задачи
+        boolean isBanned = false; // находится ли посетитель в черном списке, хардкодим согласно постановке задачи
 
-        boolean isAdult;
-        boolean isInvitedAndRich;
-        boolean isAllowedToAttend;
-        double mandatoryFee;
+        boolean isAdult; // есть ли 18 лет
+        boolean isInvitedAndRich; // приглашен ли и не находится ли в черном списке
+        boolean isAllowedToAttend; // итоговое решение, проходит пользователь или нет
+        double mandatoryFee; // обязательный добровольный взнос
 
         isAdult = age >= 18;
-        isInvitedAndRich = hasInvitation == true && accountBalance > 50000;
-        isAllowedToAttend = isAdult == true && isInvitedAndRich == true && isBanned == false;
+        isInvitedAndRich = hasInvitation && accountBalance > 50000;
+        isAllowedToAttend = isAdult && isInvitedAndRich && !isBanned;
 
-        // Форматирование до 2 знаков после запятой
-        DecimalFormat df = new DecimalFormat("0.00");
+        DecimalFormat df = new DecimalFormat("0.00"); // форматирование до 2 знаков после запятой
 
+        System.out.println("Возраст: " + age + ", Баланс счета: " + df.format(accountBalance) + " рублей"); // в постановке вывод не требуется, просто для наглядности
         System.out.println("Пользователь проходит: " + isAllowedToAttend);
-        mandatoryFee = accountBalance * 7.5 / 100;
-        System.out.println("обязательный добровольный взнос: " + df.format(mandatoryFee) + " рублей");
+        // Считаем взнос только для тех, кто проходит
+        if (isAllowedToAttend) {
+            mandatoryFee = accountBalance * 7.5 / 100;
+            System.out.println("Обязательный добровольный взнос: " + df.format(mandatoryFee) + " рублей");
+        }
     }
 }
