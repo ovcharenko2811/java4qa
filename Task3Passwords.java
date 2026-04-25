1e200bd3af9e75aff2db59c4ff21d90ffd059c83
Овчаренко Святослав Николаевич <sovcharenko@mlg.ru>
2026-04-26 01:29:59 +0400

    task 3 completed

---
+++ Task3Passwords.java
@@ -0,0 +1,13 @@
+public class Task3Passwords {
+    public static void main(String[] args) {
+        String[] passwords = {"}|{0IIA CJI0Ha"," 12332145","1qazQAZ!"};
+        for (String password : passwords){
+            boolean valid = password.length() > 8
+                    && !password.startsWith("1")
+                    && !password.endsWith("z")
+                    && !password.contains("1234")
+                    && !password.contains("qwerty");
+            System.out.println("пароль '" + password + "' прошел проверку: " + valid);
+        }
+    }
+}
