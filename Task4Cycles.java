17b3c1fc0989517267e36cabd0c9c70b4a8d4dee
Овчаренко Святослав Николаевич <sovcharenko@mlg.ru>
2026-05-04 00:48:32 +0400

    task 4 - additional tasks completed

--- Task4Cycles.java
+++ Task4Cycles.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Task4Cycles {
     static void main(String[] args) {
         // Объявляем все нужные переменные
@@ -8,6 +10,15 @@ public class Task4Cycles {
         int countBug = 0;
         int countCritical = 0;
         int countAll = 0;
+
+        // Доп. задание "Шумоподавление"
+        System.out.println("Скрыть успешные тесты? Введите: true/false"); // Исключения некорректного ввода пользователем не обрабатываются, т.к. еще не проходили
+        Scanner sc = new Scanner(System.in);
+        boolean showOnlyIssues = sc.nextBoolean();
+
+        // Доп. задание "Стоп-сигнал"
+        System.out.println("Остановить после 3 критических багов? Введите: true/false"); // Исключения некорректного ввода пользователем не обрабатываются, т.к. еще не проходили
+        boolean stopSignal = sc.nextBoolean();
 
         // Проверяем каждый номер - тест
         for(testNumber =1; testNumber<=100; testNumber++){
@@ -31,7 +42,24 @@ public class Task4Cycles {
                 countPass++;
                 countAll++;
             }
-            System.out.println("Тест #" + testNumber + ": " + testStatus);
+
+            // Продолжение доп. задания "Шумоподавления" - проверяем, отображать ли успешные тесты
+            if(showOnlyIssues == false) {
+                System.out.println("Тест #" + testNumber + ": " + testStatus);
+            }
+            else if(showOnlyIssues == true){
+                if(!testStatus.equals("Pass")){
+                System.out.println("Тест #" + testNumber + ": " + testStatus);
+                }
+            }
+
+            // Продолжение доп. задания "Стоп-сигнал" - проверяем, включен ли стоп-сигнал, передаем break циклу for, и сообщение
+            if(stopSignal == true){
+                if(countCritical == 3){
+                    System.out.println("\uD83D\uDEA8 Слишком много критических багов — будим тимлида!");
+                    break;
+                }
+            }
         }
 
         //Подводим статистику тестов
